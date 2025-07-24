8a3629455d654f947910ada8e89df506ec489bfb
EstelZoe <estelzoe433@gmail.com>
2025-07-24 13:56:33 +0000

    imported use navigate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Google from "../assets/images/google icon removed.png";
 import { FlaskConical } from 'lucide-react';
+import { useNavigate } from "react-router";
 
 export default function LoginPage() {
   const navigate = useNavigate();
